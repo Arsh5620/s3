f3d493f0142a397337763f46686c1f0e52a94442
Arshdeep Singh <arsh5620@outlook.com>
2020-02-20 04:06:17 +0000

    clean up for strings.c to only include functions that directly handles strings

--- server/strings.h
+++ server/strings.h
@@ -1,40 +1,11 @@
 # ifndef STRINGS_INCLUDE_GAURD
 # define STRINGS_INCLUDE_GAURD
 
-// #include "list.h"
-// #include "file.h"
-
-// typedef struct buffer_information {
-//     char *buffer;
-//     int length;
-//     char comment_break;
-// } string_info_s;
- 
  typedef struct string_struct {
     void *address;
-    unsigned long int length;
+    unsigned long length;
     int error;
 } string_s;
-
-// typedef struct string_process_internal {
-//     array_list_s list;
-//     char *buffer_address;
-//     char *buffer_now;
-//     char *buffer_length;
-
-//     char eof;
-// } string_internal_s;
-
-// typedef struct {
-//     char *key;
-//     char *value;
-
-//     int key_length;
-//     int value_length;
-
-//     char is_valid;
-//     char is_comment;
-// } key_value_pair_s;
 
 void tolowercase(void *memory, int length);
 int strings_count_until(char *buffer, long length, char c);
