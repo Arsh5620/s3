3fbf3607f0a881153700396fa2349172ceaaefc8
Arshdeep Singh <arsh5620@outlook.com>
2020-04-06 04:27:59 +0000

    rewrite for config.c to allow for modular processing of key value pairs to structures given the bindings between the key and the structure members and their types

--- server/config/config.c
+++ server/config/config.c
@@ -1,58 +1,106 @@
-#include "config.h"
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
+#include "config.h"
+#include "../memdbg/memory.h"
 
-key_code_pair_s config_property[5] = {
-    {"database", 8, CONFIG_DATABASE}
-    , {"machine", 7, CONFIG_MACHINE}
-    , {"password", 8, CONFIG_PASSWORD}
-    , {"port", 4, CONFIG_PORT}
-    , {"username", 8, CONFIG_USERNAME}
-};
-
-database_connection_s config_parse_dbc(char *filename)
+size_t config_search_compare(void *memory, char *str, size_t strlen)
 {
-    FILE *config    = fopen(filename, FILE_MODE_READONLY);
+    struct config_parse *address    = (struct config_parse*) memory;
+	 // order does matter
+    int cmp = memcmp(str, address->key.address, address->key.length);
 
-    database_connection_s connection = {0};
-    array_list_s list = parser_parse_file(config);
+    if(cmp == 0 && address->key.length > strlen)
+        cmp--;
+    else if(cmp == 0 && address->key.length < strlen)
+        cmp++;
+    
+    return(cmp);
+}
 
-    for(int i=0; i<list.index; ++i){
-        key_value_pair_s pair = *(key_value_pair_s*) my_list_get(list, i);
-        int configcode  = binary_search((void*) config_property
-            , sizeof(key_code_pair_s)
-            , sizeof(config_property) / sizeof(key_code_pair_s)
-            , pair.key, pair.key_length
-            , binary_search_kc_cmp);
+void config_read_all(my_list_s list, struct config_parse *configs
+	, int config_count, char *struct_memory)
+{
+	for (int i=0; i<list.count; ++i) {
+        key_value_pair_s *pair = (key_value_pair_s*) my_list_get(list, i);
+		int code  = binary_search(configs
+            , sizeof(struct config_parse)
+            , config_count
+            , pair->key, pair->key_length
+            , config_search_compare);
+		if (code >= 0) {
+			struct config_parse config	= configs[code];
+			config_copy_data(struct_memory
+				, config.offset, config.size, pair, config.type);
+		}
+	}
+}
 
-        switch (config_property[configcode].code)
-        {
-        case CONFIG_DATABASE:
-            memcpy((void*)connection.db
-                    , (void*)pair.value, pair.value_length);
-            break;
-        case CONFIG_USERNAME:
-            memcpy((void*)connection.user
-                    , (void*)pair.value, pair.value_length);
-            break;
-        case CONFIG_PASSWORD:
-            memcpy((void*)connection.passwd
-                    , (void*)pair.value, pair.value_length);
-            break;
-        case CONFIG_MACHINE:
-            memcpy((void*)connection.host
-                    , (void*)pair.value, pair.value_length);
-            break;
-        case CONFIG_PORT:
-            connection.port = (int) strtol(pair.value, NULL, 10);
-            break;
-        default:
-            // do nothing if we have a key:value pair that we don't need
-            break;
-        }
-    }
+void config_copy_data(char *struct_memory, int offset
+	, int size, key_value_pair_s *pair, enum config_data_type type)
+{
+	switch (type)
+	{
+	case CONFIG_TYPE_STRING:
+		{
+			char *memory	= 
+				m_calloc(pair->value_length + 1, MEMORY_FILE_LINE);
 
-    fclose(config);
-    parser_release_list(list);
-    return(connection);
-}   
+			/*
+			 *	first we are copying the actual pointer to the struct and 
+			 * 	then we are copying the strings.
+			 */
+			memcpy((struct_memory + offset), &memory, sizeof(char*));
+			memcpy(memory, pair->value, pair->value_length);
+		}
+		break;
+	case CONFIG_TYPE_INT:
+		{
+			*(int*)(struct_memory + offset) = 
+				(int) strtol(pair->value, NULL, 10);
+		}
+		break;
+	case CONFIG_TYPE_LONG:
+		{
+			*(long*)(struct_memory + offset) = 
+				(long) strtol(pair->value, NULL, 10);
+		}
+		break;
+	case CONFIG_TYPE_SHORT:
+		{
+			*(short*)(struct_memory + offset) = (
+					short) strtol(pair->value, NULL, 10);
+		}
+		break;
+	case CONFIG_TYPE_ENUM:
+		{
+			puts("Not yet implemented");
+			exit(1);
+		}
+		break;
+	case CONFIG_TYPE_DOUBLE:
+		{
+			*(struct_memory + offset) = strtod(pair->value, NULL);
+		}
+		break;
+	
+	default:
+		assert(type);
+		break;
+	}
+}
+
+/* 
+ *	To use the config_parse_files you will need to first create your 
+ *	bindings for the struct config_parse, and then you will pass this into 
+ *	the functions as configs. See example usage in the Config.md
+*/
+char *config_parse_files(char *filename, struct config_parse *configs
+	, int config_count, char *struct_memory) 
+{
+	FILE *config	= fopen(filename, FILE_MODE_READONLY);
+	my_list_s parsed	= parser_parse_file(config);
+	config_read_all(parsed, configs, config_count, struct_memory);
+	fclose(config);
+    parser_release_list(parsed);
+}
--- server/config/config.h
+++ server/config/config.h
@@ -6,9 +6,40 @@
 #include "../files/file.h"
 #include "../data-structures/list.h"
 #include "../parser/parser.h"
-#include "../databases/database.h"
 #include "../general/strings.h"
 #include "../general/binarysearch.h"
 
-database_connection_s config_parse_dbc(char *filename);
+enum config_data_type {
+	CONFIG_TYPE_STRING
+	, CONFIG_TYPE_INT
+	, CONFIG_TYPE_LONG
+	, CONFIG_TYPE_SHORT
+	, CONFIG_TYPE_DOUBLE
+	, CONFIG_TYPE_ENUM
+};
+
+struct config_parse {
+	string_s key;	/* key include string such as "action" */
+	int	code;	/* code for the key, must be unique */
+	int offset;	/* offset to the memory in the struct for this key */
+	int size;	/* size of the variable in the struct */
+	enum config_data_type type;
+};
+
+#define KEY_STRING(x) (string_s){.address=x, .length=sizeof(x) - 1}
+#define STRUCT_MEMBER_SIZE(STRUCT_NAME, MEMBER_NAME) \
+	sizeof(((STRUCT_NAME*)0)->MEMBER_NAME)
+#define STRUCT_CONFIG_PARSE(_key, _code, _struct, _member, _type) \
+	{ \
+		.key = KEY_STRING(_key) \
+		, .code = _code \
+		, .offset = offsetof(_struct, _member) \
+		, .size	= STRUCT_MEMBER_SIZE(_struct, _member) \
+		, .type = _type \
+	}
+
+void config_copy_data(char *struct_memory, int offset
+	, int size, key_value_pair_s *pair, enum config_data_type type);
+void config_read_all(my_list_s list, struct config_parse *configs
+	, int config_count, char *struct_memory);
 #endif // CONFIG_INCLUDE_GAURD
