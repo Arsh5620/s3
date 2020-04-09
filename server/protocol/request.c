3eb58d1fe60d44bd0c81f715f9222c4112f7216a
Arsh5620 <arsh5620@gmail.com>
2020-04-09 02:36:07 -0300

    Deleted the request.c

--- server/protocol/request.c
+++ server/protocol/request.c
@@ -2,14 +2,5 @@
 
 int dbp_request_prehook(packet_info_s *info)
 {
-	dbp_common_attribs_s attribs = info->attribs;
-	
-	if(attribs.filename.address == 0 || attribs.filename.length <= 0
-		|| attribs.folder_name.address	== 0 || attribs.folder_name.length	<= 0)
-		return(FAILED);
 
-	if(filemgmt_file_exists(&attribs.folder_name, &attribs.filename))
-		info->response.dbp_response	= DBP_RESPONSE_ACK;
-	
-	return(SUCCESS);
 }
