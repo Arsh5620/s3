0aca2c48feaa8c9917d90ded740feeb744e2cfc2
Arsh5620 <arsh5620@gmail.com>
2020-04-29 19:10:46 -0300

    Updated config to generalize use of dataparser which is responsible for parsing and converting any data received from the client.

---
+++ server/dataparser/data.h
@@ -0,0 +1,48 @@
+#ifndef CONFIG_INCLUDE_GAURD
+#define CONFIG_INCLUDE_GAURD
+
+// refer to CONFIGFORMAT for further information
+
+#include "../files/file.h"
+#include "../parser/parser.h"
+#include "../data-structures/list.h"
+#include "../data-structures/hash_table.h"
+#include "../general/binarysearch.h"
+
+enum config_data_type {
+	CONFIG_TYPE_STRING_S
+	, CONFIG_TYPE_INT
+	, CONFIG_TYPE_LONG
+	, CONFIG_TYPE_SHORT
+	, CONFIG_TYPE_BOOLEAN
+	, CONFIG_TYPE_DOUBLE
+};
+
+typedef struct {
+	char *string;
+	ulong strlen;
+	long attrib_code;
+} data_keys_s;
+
+typedef struct {
+	my_list_s list;
+	hash_table_s hash;
+} data_result_s;
+
+#define DBP_KEY(str, code) \
+	(data_keys_s) { \
+		.string = str \
+		, .strlen = sizeof(str) - 1 \
+		, .attrib_code = code \
+	}
+
+int data_convert(key_value_pair_s *pair, enum config_data_type type
+	, char *out, ulong length);
+char *data_get_key_value(hash_table_s table, long key);
+data_result_s data_parse_files(char *filename
+	, data_keys_s *keys, int key_count);
+hash_table_s data_make_table(my_list_s list
+	, data_keys_s *data, ulong length);
+size_t data_key_compare(void *memory, char *str, size_t strlen);
+void data_free(data_result_s result);
+#endif // CONFIG_INCLUDE_GAURD
